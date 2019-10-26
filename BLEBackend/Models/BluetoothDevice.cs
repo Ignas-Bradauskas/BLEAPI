11cb843643a10ad88a9a04dc6ec42bdac2cd1ec7
Ignas <ignas.bradauskas@stormgeo.com>
2019-10-26 20:42:17 +0300

    Created a backend.

---
+++ BLEBackend/Models/BluetoothDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BLEBackend.Services
+{
+    public class BluetoothDevice
+    {
+        public short SignalStrength { get; set; }
+
+        public DateTimeOffset LastSeen { get; set; }
+
+        public string Name { get; set; }
+
+        
+
+        public BluetoothDevice(short signalStrength, DateTimeOffset lastSeen, string name)
+        {
+            SignalStrength = signalStrength;
+            LastSeen = lastSeen;
+            Name = name;
+        }
+    }
+}
